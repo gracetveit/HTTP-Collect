953992d8e6a9cb16754f4570e65740b3dc41b1c5
Grace Tveit <grace.tveit@gmail.com>
2020-04-21 14:56:41 -0700

    Solved

---
+++ http-collect.js
@@ -0,0 +1,11 @@
+'use strict'
+const http = require('http');
+const bl = require('bl');
+
+http.get(process.argv[2], (res) => {
+    res.pipe(bl((err, data) => {
+        if (err) throw err;
+        console.log(data.toString().length);
+        console.log(data.toString());
+    }))
+})
